35eefb6511b74a44d3e8cd3eccab0603cb951fdd
tsubasasugawara <sugawara1740@gmail.com>
2023-02-22 07:45:38 +0000

    FIX
    Fixed install.sh.

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 flutter build apk --split-per-abi
-adb install -r -d $(PWD)/build/app/outputs/apk/release/app-arm64-v8a-release.apk
+adb install -r $PWD/build/app/outputs/apk/release/app-arm64-v8a-release.apk
